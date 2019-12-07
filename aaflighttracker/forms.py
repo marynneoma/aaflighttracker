8a7baff74453cacbf313d11b9117a59715bb8dcf
mary <ntukogumn11009@gmail.com>
2019-12-07 01:29:44 -0600

    Working Code, Needs CSS

--- aaflighttracker/forms.py
+++ aaflighttracker/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class searchform(forms.Form):
     flightorigin = forms.CharField(max_length=3, min_length=3, required=True)        # Text input for Flight Origin
     flightdestination = forms.CharField(max_length=3, min_length=3, required=True)   # Text input for Flight destination
-    flightdate = forms.DateField(input_formats='%Y-%m-%d', required=True)           # Flight Date input format "2006-10-25'
+    flightdate = forms.DateField(required=True)           # Flight Date input format "2006-10-25'
--- aaflighttracker/views.py
+++ aaflighttracker/views.py
@@ -6,4 +6,46 @@ from django import forms
 from .forms import searchform
 
 def flightviewer(request):
-    return HttpResponse("Hello Mary Python Developer")
+    apiurl = 'https://american-flight-engine-2019.herokuapp.com/flights?date='
+    origin = '&origin='
+    destination = '&destination='
+    htmltemplate = 'aaflighttracker/index.html'
+
+    if request.method == 'POST':
+        flightform = searchform(request.POST)
+        if flightform.is_valid():
+            flightorigin = flightform.cleaned_data['flightorigin']
+            flightdestination = flightform.cleaned_data['flightdestination']
+            flightdate = str(flightform.cleaned_data['flightdate'])
+            fullurl = apiurl + flightdate + origin + flightorigin + destination + flightdestination
+            jsondata = requests.get(fullurl).json()
+            args = {
+                'form': flightform,
+                'jsondata': jsondata,
+                'fullurl': fullurl,
+                'message': "Available Flights Shown Below"
+            }
+            return render(request, htmltemplate, args)
+        else:
+            flightform = searchform()
+            args = {
+                'form': flightform,
+                'message': "The Form Entries are invalid"
+            }
+            return render(request, htmltemplate, args)
+    else:
+        flightform = searchform()
+        args = {
+            'form': flightform,
+            'message': "Fill the form to search for a flight"
+        }
+        return render(request, 'aaflighttracker/index.html', args)
+    
+#    flightform = searchform()
+    
+#    htmltemplate = 'index.html'
+    
+#    context = {
+#        'form': flightform,
+#    }
+#    return render(request, htmltemplate, context)
